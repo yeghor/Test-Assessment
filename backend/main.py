1479d16edbe8b4a196adb89eb05d947a529d6670
RomanovYehor <yeghor.romanov@gmail.com>
2026-04-24 19:17:37 +0300

    Add models initialization on app startup

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
-from routers import traveling
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from routers import traveling
+from database import initialize_models, get_engine, Base
+
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global engine
+    engine = await get_engine()
+    print("Initializing models")
+    await initialize_models(engine=engine, Base=Base)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(traveling)
 
-engine = None
+# Some frontend application or Nginx
+origins = ["http://localhost:3000", "http://localhost:80"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
